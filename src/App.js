f87469418421588c17d2c4b5f29a05940374e28d
Hyun-Tae Jin <htjmario@gmail.com>
2019-04-09 14:43:29 -0400

    basic home component, material-ui, scss packages setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { Route, Redirect } from 'react-router-dom'
+
+import DesktopHome from './House/DesktopHome'
 
 class App extends Component {
   render() {
     return (
       <div>
-        
+        <Route exact path="/" render={() => <Redirect to="/home" />} />
+        <Route path="/home" component={DesktopHome} />
       </div>
     );
   }
 }
 
-export default App;
+export default App
---
+++ src/House/DesktopHome.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+import './DesktopHome.css'
+
+class DesktopHome extends Component {
+  render() {
+    return (
+      <div className='desktopDisplay'>
+        
+      </div>
+    )
+  }
+}
+
+export default DesktopHome
